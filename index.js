d2b8e965471561a3f52c6bab727de32c3c61eee4
Dasi Mohan Venkat <mohannani8328666948@gmail.com>
2023-03-25 19:19:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const json=require('json-server')
+const server=json.create()
+const router=json.router('./db.json')
+const middleware=json.defaults()
+const port=process.env.PORT || 3001
+server.use(middleware)
+server.use(router)
+server.listen(port)
